8955a1b8c8289919e13f5425028590c9f51bb562
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-10-20 16:18:48 -0400

    second commit

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -1,5 +1,42 @@
 package com.pluralsight;
 
 public class Employee {
+    private String employeeID;
+    private String employeeName;
+    private String department;
+    private double payRate;
+    private int hoursWorked;
 
+    public Employee(String employeeID, String employeeName, String department, double payRate, int hoursWorked) {
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getTotalPay(){
+        double total = getHoursWorked() * getHoursWorked();
+
+    }
 }
