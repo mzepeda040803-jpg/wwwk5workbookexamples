fe109c8892d87c0704ba10acd8d1b0962e7d2787
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-10-20 16:07:49 -0400

    initial commit

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class Application {
+
+    public static void main(String[] args) {
+        Reservation reservation1 = new Reservation("King",1,false);
+        System.out.println(reservation1.getReservationTotal());
+
+        Reservation reservation2 = new Reservation("King",1,true);
+        System.out.println(reservation2.getReservationTotal());
+    }
 }
--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,54 @@
 package com.pluralsight;
 
 public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getPrice(){
+        return switch (roomType.toLowerCase()) {
+            case "king" -> 139;
+            case "double" -> 124;
+            default -> throw new IllegalArgumentException("Invalid room type: " + roomType);
+        };
+
+    }
+
+    public double getReservationTotal(){
+        double total = getPrice() * numberOfNights;
+        if (isWeekend) {
+            total = total * 1.10;
+        }
+        return total;
+    }
 }
