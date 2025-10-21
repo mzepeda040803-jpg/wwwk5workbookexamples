85195460b51a4b3d48195ac104eab02cfd4d577c
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-10-21 10:26:00 -0400

    third commit

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -5,38 +5,42 @@ public class Employee {
     private String employeeName;
     private String department;
     private double payRate;
-    private int hoursWorked;
+    private double hoursWorked;
 
-    public Employee(String employeeID, String employeeName, String department, double payRate, int hoursWorked) {
-        this.employeeID = employeeID;
-        this.employeeName = employeeName;
-        this.department = department;
-        this.payRate = payRate;
-        this.hoursWorked = hoursWorked;
-    }
 
-    public String getEmployeeID() {
-        return employeeID;
-    }
+//    public double getTotalPay(){
+//        double totalPay = this.payRate * this.hoursWorked;
+//        return totalPay;
+//    }
 
-    public String getEmployeeName() {
-        return employeeName;
-    }
+    private double REGULAR_HOURS = 40;
+    private static final double OVERTIME_PAY_RATE = 0.15;
 
-    public String getDepartment() {
-        return department;
-    }
-
-    public double getPayRate() {
-        return payRate;
-    }
-
-    public int getHoursWorked() {
-        return hoursWorked;
+    private double getTotalPAy(){
+        return (getRegularHours() * payRate) + (getOvertimeHours() * 1 + OVERTIME_PAY_RATE);
     }
 
     public double getTotalPay(){
-        double total = getHoursWorked() * getHoursWorked();
-
+        double regularPay = getRegularHours() * payRate;
+        double overTimePay = getOvertimeHours() * OVERTIME_PAY_RATE;
+        return regularPay + overTimePay;
     }
+
+    public double getRegularHours(){
+        if (hoursWorked < 40){
+            return hoursWorked;
+        }else{
+            return REGULAR_HOURS;
+        }
+    }
+
+
+    public double getOvertimeHours(){
+        if (hoursWorked > REGULAR_HOURS){
+            return hoursWorked - REGULAR_HOURS;
+        }else{
+            return 0;
+        }
+    }
+
 }
