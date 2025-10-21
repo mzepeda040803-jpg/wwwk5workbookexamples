35115e6424e6de41bd8aa869642e79e5411541b7
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-10-21 13:55:43 -0400

    hotel added

---
+++ src/main/java/com/pluralsight/Hotel.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Hotel {
+    private String name;
+    private int numberOfSuites;
+    private int numberOfRooms;
+    private int bookedSuites;
+    private int bookedBasicRooms;
+
+
+}
